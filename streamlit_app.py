48e4de462e8f58a19917cd1c051b5c8e572be5ec
Tshajlij Lee <tlee@degenkolb.com>
2025-12-08 19:05:01 -0800

    Comment out session state write statements in main function

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ from anchor_pro.streamlit.core_ui.sidebar import render_sidebar
 from anchor_pro.streamlit.core_ui.main_page import render_main_page
 
 def main():
-    st.write(st.session_state)
+    # st.write(st.session_state)
     render_sidebar()
     render_main_page()
-    st.write(st.session_state)
+    # st.write(st.session_state)
 
 
 if __name__ == "__main__":
